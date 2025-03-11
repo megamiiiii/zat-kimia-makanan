bcabb099feab1daf7524310f65d9b01fe92f1c8a
megamiiiii <mgmegawti@gmail.com>
2025-03-11 14:06:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const pertanyaanList = [
     { kode: "C1", teks: "Apakah makanan bertekstur kenyal?" },
     { kode: "C2", teks: "Apakah makanan tidak mudah lengket?" },
     { kode: "C3", teks: "Apakah makanan berwarna mencolok?" },
-    { kode: "C4", teks: "Apakah makanan beraroma mencurigakan?" }
+    { kode: "C4", teks: "Apakah makanan beraroma mencurigakan?" },
+    { kode: "C5", teks: "Apakah makanan tidak rusak dalam 3 hari?" },
+    { kode: "C6", teks: "Apakah makanan tidak rusak dalam 15 hari?" },
+    { kode: "C7", teks: "Apakah makanan memiliki bau menyengat?" },
+    { kode: "C8", teks: "Apakah mi berwarna mengkilap?" },
+    { kode: "C9", teks: "Apakah tahu lebih keras dan kenyal?" },
+    { kode: "C10", teks: "Apakah ayam lebih padat dan keras?" },
+    { kode: "C11", teks: "Apakah insang ikan berwarna merah tua?" },
+    { kode: "C12", teks: "Apakah daging berwarna putih pucat?" },
+    { kode: "C13", teks: "Apakah makanan berwarna merah cerah?" },
+    { kode: "C14", teks: "Apakah warna makanan tidak merata?" },
+    { kode: "C15", teks: "Apakah makanan terasa pahit?" }
 ];
 
 let jawaban = [];
@@ -11,7 +22,9 @@ let indexPertanyaan = 0;
 function tampilkanPertanyaan() {
     if (indexPertanyaan < pertanyaanList.length) {
         document.getElementById("pertanyaan").innerText = pertanyaanList[indexPertanyaan].teks;
-        document.getElementById("progress").style.width = ((indexPertanyaan + 1) / pertanyaanList.length) * 100 + "%";
+        document.getElementById("progress-bar").style.width = ((indexPertanyaan + 1) / pertanyaanList.length) * 100 + "%";
+        document.getElementById("progress-bar").innerText = `${Math.round(((indexPertanyaan + 1) / pertanyaanList.length) * 100)}%`;
+        document.getElementById("progress-text").innerText = `Pertanyaan ${indexPertanyaan + 1} dari ${pertanyaanList.length}`;
     } else {
         diagnosa();
     }
